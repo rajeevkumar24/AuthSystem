aab9a2c28c8cb29025235ebe133ee27f10af1472
rajeev.k24 <rajeev.k24@outlook.com>
2020-06-13 23:01:41 +0100

    Add project files.

---
+++ AuthSystemTest/Controllers/HomeControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AuthSystem.Controllers;
+using Microsoft.Build.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AuthSystemTest
+{
+    [TestClass]
+    public class HomeControllerTest
+
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            HomeController controller = new HomeController(ILogger < HomeController > logger);
+
+
+        }
+    }
+}
---
+++ AuthSystemTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AuthSystemTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AuthSystemTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6e7367dc-ba60-44e0-af42-dce279878a5e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
